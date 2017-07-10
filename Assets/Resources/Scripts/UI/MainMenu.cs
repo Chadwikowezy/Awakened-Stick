7adafc506db5fa8b8d2c61699cbf40b9d371ff6c
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-07-09 21:06:43 -0700

    Changed button colors and added animation to the main menu.

---
+++ Assets/Resources/Scripts/UI/MainMenu.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Buttons")]
+    public GameObject playButton;
+    public GameObject equipmentButton;
+    public GameObject abilitiesButton;
+    public GameObject adButton;
+    public GameObject wimpButton;
+    public GameObject characterButton;
+    public GameObject[] allMainMenuButtons;
+
+    [Space(10), Header("Menus")]
+    public GameObject characterMenu;
+    public GameObject equipmentMenu;
+    public GameObject abilitiesMenu;
+    public GameObject[] allSubMenus;
+
+    private GameObject _currentSelectable;
+    private GameObject _currentSubMenu;
+
+    private EventSystem _currentSystem;
+
+    private void Start()
+    {
+        InitializeMenus();
+    }
+    private void Update()
+    {
+        if (_currentSystem.currentSelectedGameObject == null)
+            _currentSystem.SetSelectedGameObject(_currentSelectable);
+        else
+            _currentSelectable = _currentSystem.currentSelectedGameObject;
+    }
+
+    void InitializeMenus()
+    {
+        _currentSystem = FindObjectOfType<EventSystem>();
+        _currentSubMenu = characterMenu;
+        _currentSubMenu.GetComponent<SubMenu>().IsActiveMenu = true;
+    }
+
+    public void SetActiveSelectable(GameObject _selectable)
+    {
+        _currentSystem.SetSelectedGameObject(_selectable.gameObject);
+    }
+    public void SetActiveSubMenu(GameObject _newSubMenu)
+    {
+        _currentSubMenu = _newSubMenu;
+        _currentSubMenu.GetComponent<SubMenu>().IsActiveMenu = true;
+
+        for (int i = 0; i < allSubMenus.Length; i ++)
+            if (allSubMenus[i] != _currentSubMenu)
+                allSubMenus[i].GetComponent<SubMenu>().IsActiveMenu = false;
+    }
+
+    //Button Functions
+    public void PlayButton()
+    {
+        SetActiveSelectable(playButton);
+    }
+    public void EquipmentButton()
+    {
+        SetActiveSelectable(equipmentButton);
+        SetActiveSubMenu(equipmentMenu);
+    }
+    public void AbilitiesButton()
+    {
+        SetActiveSelectable(abilitiesButton);
+        SetActiveSubMenu(abilitiesMenu);
+    }
+    public void AdButton()
+    {
+        SetActiveSelectable(adButton);
+    }
+    public void WimpButton()
+    {
+        SetActiveSelectable(wimpButton);
+    }
+    public void CharacterButton()
+    {
+        SetActiveSelectable(characterButton);
+        SetActiveSubMenu(characterMenu);
+    }
+}
---
+++ Assets/Resources/Scripts/UI/SubMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMenu : MonoBehaviour
+{
+    private bool _isActiveMenu;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public bool IsActiveMenu
+    {
+        get { return _isActiveMenu; }
+        set
+        {
+            bool previousActiveState = _isActiveMenu;
+            bool newActiveState = value;
+
+            if (previousActiveState == false && newActiveState == true)
+                _anim.Play("SubMenu_MoveIn");
+            else if (previousActiveState == true && newActiveState == false)
+                _anim.Play("SubMenu_MoveOut");
+
+            _isActiveMenu = newActiveState;
+        }
+    }
+}
