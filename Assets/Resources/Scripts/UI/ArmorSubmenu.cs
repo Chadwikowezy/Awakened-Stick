373b038bd989cb6a5670addc5dad37dac2481430
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-07-24 15:56:45 -0700

    - Fixed scaling issues with character menu by doing a complete rework of the hierarchy and anchor point structure. - Re-aranged player and character stats

---
+++ Assets/Resources/Scripts/UI/ArmorSubmenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorSubmenu : MonoBehaviour
+{
+    private enum ArmorMenus { Helmet, Armor, Gloves }
+
+    private ArmorMenus _currentArmorMenu;
+
+    public GameObject helmetButton;
+    public GameObject armorButton;
+    public GameObject glovesButton;
+
+    public GameObject helmetsScrollView;
+    public GameObject armorScrollView;
+    public GameObject glovesScrollview;
+
+    private void Start()
+    {
+        SetActiveArmorMenu(ArmorMenus.Helmet);
+    }
+
+    void SetActiveArmorMenu(ArmorMenus _newActiveMenu)
+    {
+        if (_newActiveMenu == ArmorMenus.Helmet)
+        {
+            helmetButton.GetComponent<Image>().color = Color.black;
+            armorButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+            glovesButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+        }
+        else if (_newActiveMenu == ArmorMenus.Armor)
+        {
+            helmetButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+            armorButton.GetComponent<Image>().color = Color.black;
+            glovesButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+        }
+        else if (_newActiveMenu == ArmorMenus.Gloves)
+        {
+            helmetButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+            armorButton.GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+            glovesButton.GetComponent<Image>().color = Color.black;
+        }
+
+        _currentArmorMenu = _newActiveMenu;
+    }
+
+    public void HelmetButton()
+    {
+        SetActiveArmorMenu(ArmorMenus.Helmet);
+    }
+    public void ArmorButton()
+    {
+        SetActiveArmorMenu(ArmorMenus.Armor);
+    }
+    public void GlovesButton()
+    {
+        SetActiveArmorMenu(ArmorMenus.Gloves);
+    }
+}
