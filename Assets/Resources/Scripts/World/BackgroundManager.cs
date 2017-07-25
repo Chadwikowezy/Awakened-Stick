88989bc0bb5a433e13f33a2e353b1fb80037fbbd
Steven Moreno <stevenxmoreno@gmail.com>
2017-07-25 14:53:44 -0700

    Added background randomizer

---
+++ Assets/Resources/Scripts/World/BackgroundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour 
+{
+	public SpriteRenderer backgroundSpriteRenderer;
+	public Sprite[] backgroundSprites;
+
+	void Start () 
+	{
+		backgroundSpriteRenderer.sprite = backgroundSprites [Random.Range (0, backgroundSprites.Length)];
+
+	}
+
+}
