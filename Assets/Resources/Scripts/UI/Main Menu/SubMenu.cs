4ef21551cf4894a05d1ef09e952b9512690a590b
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-08-01 13:55:06 -0700

    - Fixed menu slide in animation (anim transition was happening to early) - Added in weapons tab to equipment sub menu - Made armor and weapons side tabs work on the equipment menu

--- Assets/Resources/Scripts/UI/Main Menu/SubMenu.cs
+++ Assets/Resources/Scripts/UI/Main Menu/SubMenu.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SubMenu : MonoBehaviour
 {
+    public GameObject[] tabs;
+    public GameObject[] tabMenus;
+
     private bool _isActiveMenu;
     private Animator _anim;
+    private GameObject _activeTab;
 
     private void Start()
     {
@@ -30,5 +35,34 @@ public class SubMenu : MonoBehaviour
 
             _isActiveMenu = newActiveState;
         }
+    }
+
+    public void TabOne()
+    {
+        for (int i = 0; i < tabs.Length; i++)
+        {
+            tabs[i].GetComponent<Image>().color = new Color(0.4f, 0.4f, 0.4f, 1f);
+            tabMenus[i].SetActive(false);
+        }
+
+        tabs[0].GetComponent<Image>().color = Color.white;
+        tabs[0].transform.SetSiblingIndex(transform.parent.childCount);
+        tabMenus[0].SetActive(true);
+    }
+    public void TabTwo()
+    {
+        for (int i = 0; i < tabs.Length; i++)
+        {
+            tabs[i].GetComponent<Image>().color = new Color(0.4f, 0.4f, 0.4f, 1f);
+            tabMenus[i].SetActive(false);
+        }
+
+        tabs[1].GetComponent<Image>().color = Color.white;
+        tabs[1].transform.SetSiblingIndex(transform.parent.childCount);
+        tabMenus[1].SetActive(true);
+    }
+    public void TabThree()
+    {
+
     }
 }
