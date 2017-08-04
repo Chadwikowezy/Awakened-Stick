19a737d85e1cb55495021aaf757e5cbd6ca07513
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-08-04 15:01:02 -0700

    - added vertical layout group and content size filter to armor submenus - created items in armor submenus (no functionality yet)

--- Assets/Resources/Scripts/UI/Main Menu/SubmenuTabs.cs
+++ Assets/Resources/Scripts/UI/Main Menu/SubmenuTabs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ArmorSubmenu : MonoBehaviour
+public class SubmenuTabs : MonoBehaviour
 {
     private enum ArmorMenus { Helmet, Armor, Gloves }
 
