2c8f72ee1c560c0e9a154088c3b157f7999c5c60
Chadwikowezy <chad_mcleavy@hotmail.com>
2017-08-16 18:38:02 -0700

    - Layed out ability description UI - Added code architecture to display ability description - Added ability metadata script to ascending shot ability.
    Still need to add metadata script to other ability buttons.

---
+++ Assets/Resources/Scripts/UI/Main Menu/AbilityDescriptionWindow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityDescriptionWindow : MonoBehaviour
+{
+    public Image skillIcon;
+    public Text skillName;
+    public Text skillType;
+    public Text castType;
+    public Text damage;
+    public Text description;
+
+    public void SetDisplayedMetaData(AbilityMenuItem _ability)
+    {
+        skillIcon.sprite = _ability.skillIcon;
+        skillName.text = _ability.skillName;
+        skillType.text = _ability.skillType;
+        castType.text = _ability.castType;
+        damage.text = _ability.damage;
+        description.text = _ability.description;
+    }
+}
---
+++ Assets/Resources/Scripts/UI/Main Menu/AbilityMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityMenuItem : MonoBehaviour
+{
+    public Sprite skillIcon;
+    public string skillName;
+    public string skillType;
+    public string castType;
+    public string damage;
+    public string description;
+
+    public AbilityDescriptionWindow _descriptionWindow;
+
+    public void UpdateAbilityDisplayWindow()
+    {
+        _descriptionWindow.gameObject.SetActive(true);
+        _descriptionWindow.SetDisplayedMetaData(this);
+    }
+}
